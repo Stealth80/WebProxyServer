2c27f08dd347142550feb809b14b31f59fe9bacd
Stealth80 <Stealth80_@hotmail.com>
2013-12-03 22:48:30 -0500

    Connection, Proxy, and Logfile

--- proxy.c
+++ proxy.c
@@ -17,19 +17,18 @@
  */
 int parse_uri(char *uri, char *target_addr, char *path, int  *port);
 void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
-void handle_request(int connfd, struct sockaddr_in *sockaddr);
+int handle_request(int connfd, struct sockaddr_in *sockaddr);
 
 /* 
  * main - Main routine for the proxy program 
  */
 int main(int argc, char **argv)
 {     
-	int listenfd, connfd, port, clientlen, serverPort; //listenfd for listening descriptor, connfd for connected descriptor
+	int listenfd, connfd, port, clientlen; //listenfd for listening descriptor, connfd for connected descriptor
 	struct sockaddr_in clientaddr;
 	struct hostent *hp;	//pointer to DNS host entry
-	struct sockaddr_in serveraddr;
 	char *haddrp;	//pointer to dotted decimal string
-	unsigned short client_port;
+	//unsigned short client_port;
 
     /* Check arguments */
     if (argc != 2) {
@@ -38,18 +37,22 @@ int main(int argc, char **argv)
     }
 
 	port = atoi(argv[1]);  //listens on port passed on the command line
-	listenfd = open_listenfd(port);   //listening descriptor
+	listenfd = Open_listenfd(port);   //listening descriptor
 
 	while(1) {
 		clientlen = sizeof(clientaddr);
-		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-		hp = Gethosebyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clentaddr.sin_addr.s_addr), AF_INET);
+		connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+		hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
 		haddrp = inet_ntoa(clientaddr.sin_addr);
-		client_port = ntohs(clientaddr.sin_port);
+		printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+		//client_port = ntohs(clientaddr.sin_port);
 
 		if(fork() == 0) { //if child
 			Close(listenfd); //close listen socket
-			handle_request(connfd, &clientaddr); //handle request
+			if(handle_request(connfd, &clientaddr) < 0) //handle request
+			{
+				printf("Error Handling Request");
+			}
 			exit(0);  //on exit will close remaining fd and child ends
 		}
 		else  //if parent
@@ -62,54 +65,81 @@ int main(int argc, char **argv)
 }
 
 
-void handle_request(int connfd, struct sockaddr_in *sockaddr)
+int handle_request(int connfd, struct sockaddr_in *sockaddr)
 {
-	int clientfd, port, size, bufSize;
-	char *host, buf[MAXLINE], uri[MAXLINE], version[MAXLINE];
+	int clientfd, port;
+	int bufSize=0;
+	char buf[MAXLINE], uri[MAXLINE], version[MAXLINE], logstring[MAXLINE], method[MAXLINE];
 	rio_t rio;
 	char hostname[MAXLINE];
 	char pathname [MAXLINE];
+	char msg [MAXLINE];
+	size_t m, n;
+	FILE *fp;
 
 	Rio_readinitb(&rio, connfd); //connection to client for reading
-	bufSize = Rio_redlineb(&rio, buf, MAXLINE); //read from client
+	n = Rio_readlineb(&rio, buf, MAXLINE); //read from client
 	sscanf(buf, "%s %s %s", method, uri, version);  //scan input from client and extract method, uri, and version
 
-	if(strcmp(method, "GET")) != 0)
+	if(strcmp(method, "GET") != 0)
 	{
 		printf("Invalid Method. \n");
-		return;
+		return -3;
 	}
 
 	parse_uri(uri, hostname, pathname, &port);  //call parse_uri to extract host name, path name, and port
-
+	printf("method = %s, version = %s, uri: %s, hostname = %s, pathname = %s, port = %d", method, version, uri, hostname, pathname, port);
 	//check URL against cached URL list
-	if() //if cached already
+	if(0) //if cached already
 	{
 		
 	}
 	else //if not cached already
 	{
 		//if((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) //open connection to end server
-		if((clientfd = Open_clientfd(hostname, port);
+		if((clientfd = Open_clientfd(hostname, port)) < 0)
+		{
+			printf("%d ", clientfd);
 			return -1; 
+		}
 		if(hostname == NULL)
 			return -2;
 		
+		Rio_writen(clientfd, buf, n);
+		Rio_writen(clientfd, "\n", 1);
+
+		Rio_readinitb(&rio, clientfd);
 		//strcat(buf, "\n"); //add end line
 		//Rio_writen(clientfd, buf, bufSize+1);  //send client request to server
 
 		//printf("type:");
-		fflush(stdout);
-		while(Fgets(buf, MAXLINE, stdin) ! = NULL) { //read input line from client
-			size += strlen(buf);
-			Rio_writen(clientfd, buf, strlen(buf)); //send line to server
-			Rio_readlineb(&rio, buf, MAXLINE);  //receive line back from server
+		//fflush(stdout);
+		//while(Fgets(buf, MAXLINE, stdin) ! = NULL) { //read input line from client
+		//	size += strlen(buf);
+		//	Rio_writen(clientfd, buf, strlen(buf)); //send line to server
+		//	Rio_readlineb(&rio, buf, MAXLINE);  //receive line back from server
+		//}
+		while ((m = Rio_readn(clientfd, msg, MAXLINE)) > 0) {
+			Rio_writen(connfd, msg, m);
+			/*sum the total number of bytes written */
+			bufSize += m;
 		}
-		Close(clientfd);
 
 		//write log entry to log file
-		format_log_entry(logstring, sockaddr, uri, size);
+		format_log_entry(logstring, sockaddr, uri, bufSize);
+		printf("%s\n",logstring);
+		fp = fopen("proxy.log", "a");
+		if (!fp) {
+			printf("Log not written!");
+		}
+		else {
+			fprintf(fp, "%s\n", logstring);
+			fclose(fp);
+		}
+
+		Close(clientfd);
 	}
+	return 0;
 }
 /*
  * parse_uri - URI parser
@@ -189,7 +219,14 @@ void format_log_entry(char *logstring, struct sockaddr_in *sockaddr,
 
 
     /* Return the formatted log entry string */
-    sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
+	if(size < 1)
+	{
+		sprintf(logstring, "%s: %d.%d.%d.%d %s (NOTFOUND)", time_str, a, b, c, d, uri);
+	}
+	else
+	{
+		sprintf(logstring, "%s: %d.%d.%d.%d %s %d", time_str, a, b, c, d, uri, size);
+	}
 }
 
 
